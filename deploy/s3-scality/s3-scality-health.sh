e4bbb2f92170eee96f5d1a8a002e7ba940928baf
Rom Freiman <rom@stratoscale.com>
2017-08-08 21:54:31 +0300

    Adjust healthcheck and wrapper to work with s3-manager

--- deploy/s3-scality/s3-scality-health.sh
+++ deploy/s3-scality/s3-scality-health.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+S3_MANAGER_URL="http://s3-manager-api.service.strato:7540/api/v2/s3-manager/volumes"
 
-S3_KV_PATH="s3-scality/v1/s3-volume"
-
-if [[ `consul kv get "$S3_KV_PATH" > /dev/null 2>&1; echo $?` -ne "0" ]]; then
+if [[ `curl -s "$S3_MANAGER_URL" | grep -qE 'Ready'; echo $?` -ne "0" ]]; then
     docker inspect -f {{.State.Running}} s3-scality.service.strato > /dev/null 2>&1;
 else
     curl -s http://127.0.0.1:8800/_/healthcheck > /dev/null 2>&1;
